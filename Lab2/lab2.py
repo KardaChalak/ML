f21be5bd3367f3c2c656b3f307ae754919bfcc7b
Sandra <sandra.backstrom@gmail.com>
2017-02-20 19:12:26 +0100

    Code works, makes pretty plots sometimes..

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -4,6 +4,7 @@ from cvxopt.base import matrix
 
 ###### FUNCTIONS ######
 
+# Generates the data
 def generateTestData():
 
     classA = [(random.normalvariate(-1.5, 1),
@@ -25,7 +26,8 @@ def generateTestData():
 
     return data, classA, classB
 
-def plot(classA, classB):
+# Plots the initial data points and their class by color
+def plotData(classA, classB):
 
     pylab.hold(True)
     pylab.plot ([p[0] for p in classA],
@@ -38,8 +40,44 @@ def plot(classA, classB):
     pylab.show()
     return
 
+# Plots the initial data points and their class by color
+def plotDecisionBoundary(classA, classB, non_zero_alpha):
 
-# Returns the scalar product between two points + 1#
+    pylab.hold(True)
+    pylab.plot([p[0] for p in classA],[p[1] for p in classA], "bo" ) 
+    pylab.plot([p[0] for p in classB],[p[1] for p in classB], "ro" )
+    pylab.plot([p[0][0] for p in non_zero_alpha],[p[0][1] for p in non_zero_alpha], "go" )
+
+    xrange=numpy.arange(-4, 4, 0.05) 
+    yrange=numpy.arange(-4, 4, 0.05)
+    grid=matrix([[indicator(non_zero_alpha,[x,y,1.0]) for y in yrange] for x in xrange])
+
+    pylab.contour(xrange, yrange, grid ,(-1.0, 0.0, 1.0), colors=("red", "black", "blue"), linewidths=(1, 3, 1))
+    pylab.show()
+    return
+
+# ti and tj represents the target class for datapoint i and j.
+def buildPmatrix(data):
+
+    P = numpy.zeros((len(data), len(data)))
+    for i in range(0, len(data)):
+        ignore, ignore, ti = data[i]
+        for j in range(0, len(data)):
+            ignore, ignore, tj = data[j]
+            P[i, j] = ti * tj * kernelFunction(data[i], data[j])
+
+    return P
+
+# Takes out the (basically) 0 values from alpha
+def findNonZeroAlpha(alpha, data):
+  non_zero_alpha = []
+  for i, val in enumerate(alpha):
+    if val > 10**(-5):
+      non_zero_alpha.append((data[i][:], val))
+
+  return non_zero_alpha
+
+# Kernel functions:
 def linearKernel(point1, point2):
 
     (x1, y1, ignore) = point1
@@ -47,27 +85,69 @@ def linearKernel(point1, point2):
 
     return x1 * x2 + y1 * y2 + 1
 
-# ti and tj represents the target class for datapoint i and j.
-def buildPmatrix(data):
+def polynomialKernel(point1, point2, power):
 
-    P = numpy.zeros((len(data), len(data)))
-    for i in range(0, len(t)):
-        ignore, ignore, ti = data[i]
-        for j in range(0, len(t)):
-            ignore, ignore, tj = data[j]
-            P[i, j] = ti * tj * linearKernel(data[i], data[j])
+  return linearKernel(point1, point2)**power
 
-    return P
+def radialKernel(point1, point2, sigma):
+
+    (x1, y1, ignore) = point1
+    (x2, y2, ignore) = point2
+
+    sum = (x1-x1)**2 + (y1-y2)**2
+
+    return math.exp(-sum/(2*sigma**2))
+
+def sigmoidKernel(point1, point2, k, delta):
+    (x1, y1, ignore) = point1
+    (x2, y2, ignore) = point2
+
+    sum = x1*x2 + y1*y2
+
+    return math.tanh(k*sum-delta)
+
+
+# Define here what kernel function you want to use for the program!!! <--- KERNEL FUNCTION
+def kernelFunction(point1, point2):
+  power = 2;
+  sigma = 2;
+  k = 0.005;
+  delta = 0.01;
+
+  #return linearKernel(point1, point2)
+  return polynomialKernel(point1, point2, power)
+  #return radialKernel(point1, point2, sigma)
+  #return sigmoidKernel(point1, point2, k, delta)
+
+def indicator(non_zero_alpha, points):
+    sum = 0
+    for coordinates, alpha in non_zero_alpha:
+      sum = sum + alpha * coordinates[2] * kernelFunction(coordinates, points) 
+
+    return sum
+
+
 
 ##### Main script #####
 
 data, classA, classB = generateTestData()
-plot(classA, classB)
+
+plotData(classA, classB)
+
 
 P = buildPmatrix(data)
+q = numpy.ones(len(data)) * -1
+h = numpy.zeros(len(data))
+G = numpy.diag(q)
 
-#print(len(P))
-print(P)
+r = qp(matrix(P) , matrix(q) , matrix(G) , matrix(h))
+alpha = list(r['x'])
+
+non_zero_alpha = findNonZeroAlpha(alpha, data)
+
+plotDecisionBoundary(classA, classB, non_zero_alpha)
+
+
 
 
 
